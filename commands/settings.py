61cbfe9dfb38e73b8b57529f243150ed3160d2a6
reiKloske <lanafofa2@gmail.com>
2024-08-17 18:52:39 +0100

    Settings command improvement - Showing Enabled/Disabled instead of True/False

--- commands/settings.py
+++ commands/settings.py
@@ -6,9 +6,10 @@ class Settings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
+    # Showing which commands are enabled/disabled
     @commands.command()
     async def settings(self, ctx):
-        settings_message = "**Enabled Commands:**\n" + "\n".join([f"{cmd}: **{status}**" for cmd, status in enabled_commands.items()])
+        settings_message = "**Enabled Commands:**\n" + "\n".join([f'{cmd}: **{"Enabled" if status else "Disabled"}**' for cmd, status in enabled_commands.items()])
         await ctx.send(settings_message)
 
 
